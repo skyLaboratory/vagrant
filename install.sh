738f763046e8d27d68444be684b358522ad16299
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-04-19 01:38:04 +0200

    improved install.sh
    - added apache2 install
    - added php7 install
    - added composer install
    - added php-fpm config

--- install.sh
+++ install.sh
@@ -1,9 +1,40 @@
+# Some stuff
+sudo apt-get install -y nano wget python-software-properties htop
+
+# PHP7
+sudo apt-get install -y language-pack-en-base
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt-get update
+apt-get install -y php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-dev php7.0-intl php7.0-gd php7.0-curl --force-yes
+
+
+#Apache
+apt-get install -y apache2
+echo "ServerName localhost" >> /etc/apache2/httpd.conf
+apt-get install -y apache2-mpm-worker 
+
+# php fpm config
+cat > /etc/apache2/sites-available/000-default.conf <<EOF
+DirectoryIndex index.php
+<LocationMatch "^(.*\.php)$">
+     ProxyPass fcgi://127.0.0.1:9000/var/www/
+</LocationMatch>
+EOF
+cat /etc/apache2/sites-available/000-default.conf.2  >> /etc/apache2/sites-available/000-default.conf
+rm /etc/apache2/sites-available/000-default.conf.2
+sed -i "s/listen = \/run\/php\/php7.0-fpm.sock/listen = 127.0.0.1:9000/" /etc/php/7.0/fpm/pool.d/www.con
+
+# Mariadb
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-sudo apt-get install -y nano wget python-software-properties apache2
 sudo apt-get update
 sudo apt-get -y install mysql-server
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo /etc/init.d/mysql restart
+
+#composer
+php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"
+mv -f composer.phar /usr/local/bin/composer.phar
+alias composer='/usr/local/bin/composer.phar'
+
