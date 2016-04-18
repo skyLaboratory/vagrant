2ae07807c3504a2081de3beeb6ceb90efd495e46
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-04-18 17:38:59 +0200

    vagrant init
    - added initial vagrant file
    - added first version of provision script

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y nano wget python-software-properties apache2
+sudo apt-get update
+sudo apt-get -y install mysql-server
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+sudo /etc/init.d/mysql restart
